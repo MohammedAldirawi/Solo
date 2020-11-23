a6665f265172b6deb74ce37f339e2db4b3c9aca8
mohtareq <mtareq571@gmail.com>
2020-11-23 04:19:44 +0200

    team done

--- js/js.js
+++ js/js.js
@@ -4,3 +4,29 @@ $(window).on('load', function() {
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+
+// Team
+
+$(function() {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
+
+// Progress Bars
+
+$(function() {
+    $('#progress-elements').waypoint(function() {
+        $('.progress-bar').each(function() {
+            $(this).animate({ width: $(this).attr('aria-valuenow') + '%' }, 1000);
+        });
+        this.destroy();
+    }, { offset: 'bottom-in-view' });
+});
