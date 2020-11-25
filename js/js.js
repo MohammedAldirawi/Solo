c0534b2968624b83e44ea1391cdf42753266b53a
mohtareq <mtareq571@gmail.com>
2020-11-26 01:41:43 +0200

    20 Done

--- js/js.js
+++ js/js.js
@@ -96,3 +96,17 @@ $(function() {
         time: 1000
     });
 });
+
+// Clients
+$(function() {
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: false,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
