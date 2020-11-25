3355ed0a969ea58fae1bccdc05e3f65f8b8cb461
mohtareq <mtareq571@gmail.com>
2020-11-25 05:02:57 +0200

    17 Done

--- js/js.js
+++ js/js.js
@@ -71,3 +71,17 @@ $(function() {
         }
     });
 });
+
+// Testimonial
+$(function() {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        autoplay: false,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
