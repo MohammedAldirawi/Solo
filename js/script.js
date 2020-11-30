732c5209c88807d7e4c1280bb2cbc57448283e6f
mohtareq <mtareq571@gmail.com>
2020-11-30 18:37:25 +0200

    still map

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-// Preloader
-
-$(window).on('load', function() {
+/* =========================================
+                Preloader
+============================================ */
+$(window).on('load', function() { // makes sure that whole site is loaded
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
 
-// Team
 
+/* =========================================
+                Team
+=========================================== */
 $(function() {
     $("#team-members").owlCarousel({
         items: 2,
@@ -16,65 +19,96 @@ $(function() {
         autoplayHoverPause: true,
         nav: true,
         dots: false,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 2
+            }
+        }
     });
 });
 
-
-
-// Progress Bars
-
+/* =========================================
+                Progress Bars
+============================================ */
 $(function() {
-    $('#progress-elements').waypoint(function() {
-        $('.progress-bar').each(function() {
-            $(this).animate({ width: $(this).attr('aria-valuenow') + '%' }, 1000);
+
+    $("#progress-elements").waypoint(function() {
+
+        $(".progress-bar").each(function() {
+
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 2000);
+
         });
+
         this.destroy();
-    }, { offset: 'bottom-in-view' });
+    }, {
+        offset: 'bottom-in-view'
+    });
+
 });
 
-
-// Responsive Tabs
+/* =========================================
+               Responsive Tabs
+============================================ */
 $(function() {
-    $('#services-tabs').responsiveTabs({
+
+    $("#services-tabs").responsiveTabs({
         animation: 'slide'
     });
+
 });
 
 
-// Portfolio
-
+/* =========================================
+               Portfolio
+============================================ */
 $(window).on('load', function() {
-    // Initialize
-    $("#isotope-container").isotope({
 
-    });
-    // Filter Items
+    // Initialize Isotope
+    $("#isotope-container").isotope({});
+
+    // filter items on button click
     $("#isotope-filters").on('click', 'button', function() {
-        // Git filter value
+
+        // get filter value
         var filterValue = $(this).attr('data-filter');
-        // Filter Portfolio
+
+        // filter portfolio
         $("#isotope-container").isotope({
             filter: filterValue
         });
+
         // active button
         $("#isotope-filters").find('.active').removeClass('active');
         $(this).addClass('active');
     });
 });
-
-// Popup
+/* =========================================
+               Magnifier
+============================================ */
 $(function() {
-    $('#portfolio-wrapper').magnificPopup({
-        delegate: 'a',
+
+    $("#portfolio-wrapper").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
         type: 'image',
         gallery: {
             enabled: true
         }
     });
+
 });
 
-// Testimonial
+/* =========================================
+               Testimonials
+============================================ */
 $(function() {
     $("#testimonial-slider").owlCarousel({
         items: 1,
@@ -88,16 +122,23 @@ $(function() {
     });
 });
 
-// Stats Counter
 
+/* =========================================
+              Stats
+============================================ */
 $(function() {
-    $('.counter').counterUp({
+
+    $(".counter").counterUp({
         delay: 10,
-        time: 1000
+        time: 2000
     });
+
 });
 
-// Clients
+
+/* =========================================
+              Clients
+============================================ */
 $(function() {
     $("#clients-list").owlCarousel({
         items: 6,
@@ -107,93 +148,165 @@ $(function() {
         autoplayHoverPause: true,
         nav: true,
         dots: false,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 2
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 3
+            },
+            // breakpoint from 768 up
+            768: {
+                items: 6
+            }
+        }
     });
 });
 
-// Map
 
+/* =========================================
+              Google Map
+============================================ */
 $(window).on('load', function() {
-    // Map Vars
-    var addressString = '230 Broadway, NY, New York 10007 USA';
-    var myLating = {
-        let: 40.712685,
+
+    // Map Variables
+    var addressString = '230 Broadway, NY, New York 10007, USA';
+    var myLatlng = {
+        lat: 40.712685,
         lng: -74.005920
     };
+
     // 1. Render Map
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 11,
-        center: myLating
+        center: myLatlng
     });
+
     // 2. Add Marker
     var marker = new google.maps.Marker({
-        position: myLating,
+        position: myLatlng,
         map: map,
-        title: 'Click To See Address'
+        title: "Click To See Address"
     });
+
     // 3. Add Info Window
     var infowindow = new google.maps.InfoWindow({
         content: addressString
     });
-    // Show Info Window when user clicks marker
+
+    // Show info window when user clicks marker
     marker.addListener('click', function() {
         infowindow.open(map, marker);
     });
+
+    // 4. Resize Function
+    google.maps.event.addDomListener(window, 'resize', function() {
+
+        var center = map.getCenter();
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(center);
+
+    });
+
 });
 
-// Navigation => White Nav
+
+/* =========================================
+              Navigation
+============================================ */
+
+/* Show & Hide White Navigation */
 $(function() {
-    // show/hide nav on load
+
+    // show/hide nav on page load
     showHideNav();
+
     $(window).scroll(function() {
-        // show/hide nav on scroll
+
+        // show/hide nav on window's scroll
         showHideNav();
     });
 
     function showHideNav() {
+
         if ($(window).scrollTop() > 50) {
-            // Show White Nav
-            $("nav").addClass('white-nav-top');
-            // Show Dark Logo
+
+            // Show white nav
+            $("nav").addClass("white-nav-top");
+
+            // Show dark logo
             $(".navbar-brand img").attr("src", "img/logo/logo-dark.png");
-            // Show Back To Top Button
+
+            // Show back to top button
             $("#back-to-top").fadeIn();
+
         } else {
-            // Hide White Nav
+
+            // Hide white nav
             $("nav").removeClass("white-nav-top");
-            // Show Logo
+
+            // Show logo
             $(".navbar-brand img").attr("src", "img/logo/logo.png");
-            // Hide Back To Top Button
+
+            // Hide back to top button
             $("#back-to-top").fadeOut();
         }
     }
 });
 
 // Smooth Scrolling
-
 $(function() {
-    $("a.smmoth-scroll").click(function(event) {
+
+    $("a.smooth-scroll").click(function(event) {
+
         event.preventDefault();
-        // Get Section Id
+
+        // get section id like #about, #servcies, #work, #team and etc.
         var section_id = $(this).attr("href");
+
         $("html, body").animate({
             scrollTop: $(section_id).offset().top - 64
         }, 1250, "easeInOutExpo");
+
     });
+
 });
 
+/* =========================================
+              Mobile Menu
+============================================ */
+$(function() {
 
+    // Show mobile nav
+    $("#mobile-nav-open-btn").click(function() {
+        $("#mobile-nav").css("height", "100%");
+    });
 
+    // Hide mobile nav
+    $("#mobile-nav-close-btn, #mobile-nav a").click(function() {
+        $("#mobile-nav").css("height", "0%");
+    });
 
+});
 
+/* =========================================
+                Animation
+============================================ */
+// animate on scroll
+$(function() {
+    new WOW().init();
+});
 
+// home animation on page load
+$(window).on('load', function() {
 
-/*
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLing(51.508742, -0.120850),
-        zoom: 5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
-*/
+    $("#home-heading-1").addClass("animated fadeInDown");
+    $("#home-heading-2").addClass("animated fadeInLeft");
+    $("#home-text").addClass("animated zoomIn");
+    $("#home-btn").addClass("animated zoomIn");
+    $("#arrow-down i").addClass("animated fadeInDown infinite");
+
+});
