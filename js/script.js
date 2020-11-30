569fbeef86f189611ba71aa818ca3aafa481a350
mohtareq <mtareq571@gmail.com>
2020-11-30 16:36:27 +0200

    back again

--- js/script.js
+++ js/script.js
@@ -16,15 +16,7 @@ $(function() {
         autoplayHoverPause: true,
         nav: true,
         dots: false,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        responsive: {
-            0: {
-                items: 1
-            },
-            489: {
-                items: 2
-            }
-        }
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
     });
 });
 
